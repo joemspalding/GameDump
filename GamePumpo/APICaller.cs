a477f264ac3bd9e05a1dac57103cd7c4aad4fa33
joemspalding <jmspal11@louisville.edu>
2018-04-07 20:50:49 -0400

    updated file structure

---
+++ GamePumpo/APICaller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePumpo
+{
+    public static class APICaller
+    {
+
+        /// <summary>
+        /// Make a GET request to a web API async
+        /// </summary>
+        /// <param name="URL"></param>
+        /// <returns></returns>
+        public static async Task<string> ApiCallAsync(string URL)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(URL);
+                    using (HttpResponseMessage message = client.GetAsync(URL).Result)
+                    {
+                        using (HttpContent response = message.Content)
+                        {
+                            return await response.ReadAsStringAsync();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return "-1";
+        }
+    }
+}
---
+++ GamePumpo/Abstract/IGameGetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePumpo
+{
+    public interface IGameGetter
+    {
+        List<Game> GetGames();
+    }
+}
