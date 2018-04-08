5e10f83c67f7611016846936ccfe03a1b27b14dd
joemspalding <jmspal11@louisville.edu>
2018-04-08 00:30:40 -0400

    Added Nintendo Resources for starting

---
+++ GamePumpo/Systems/NintendoGameGetter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePumpo.Systems
+{
+    class NintendoGameGetter
+    {
+        //Useful links
+        //
+        // Nintendo Games API
+        // https://www.nintendo.com/json/content/get/filter/game?system=switch
+        // There are more parameters to mess with
+        //
+        // Unofficial Nintendo Node API 
+        // https://github.com/andreyazevedo/eshop-api-wrapper/blob/master/src/index.js
+        // Lots of very detailed information on what to expect from the Nintendo API
+    }
+}
